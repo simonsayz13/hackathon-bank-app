9d13e821df2e70caa026b02522525b2379e3cafe
Simon Tan <simontan@MacBook-Pro.local>
2024-11-30 00:34:16 +0000

    remove unused theme

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -16,7 +16,6 @@ import { useColorScheme } from "@/hooks/useColorScheme";
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
